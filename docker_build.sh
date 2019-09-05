951303e9f62a06ed81cbf1c2c62990bb2740c071
aciddude <david@walton.io>
2019-09-05 19:11:24 +0100

    updated docker build script

--- docker_build.sh
+++ docker_build.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 # This file tags and uploads an image to Docker Hub
 
+export BUILDID=$1
 
 # Step 0:
 # Docker build
-docker build -t acidd/udacity-weather-app:latest -t acidd/udacity-weather-app:v1 .
+docker build -t acidd/udacity-weather-app:latest  -t acidd/udacity-weather-app:$BUILDID .
+
 
 # Step 1:
 # Create dockerpath
-dockerpath=acidd/udacity-weather-app:v1
+dockerpath=acidd/udacity-weather-app:$BUILDID
 
 # Step 2:
 # Authenticate & tag
@@ -16,10 +18,10 @@ echo "Docker ID and Image: $dockerpath"
 
 export DOCKER_ID_USER="acidd"
 docker login
-docker tag udacity-weather-app:v1 $DOCKER_ID_USER/udacity-weather-app:v1
-docker tag udacity-weather-app:latest $DOCKER_ID_USER/udacity-weather-app:v1
 
 
 # Step 3:
 # Push image to a docker repository
+docker tag $DOCKER_ID_USER/udacity-weather-app:latest $DOCKER_ID_USER/udacity-weather-app:$BUILDID
 docker push $DOCKER_ID_USER/udacity-weather-app:latest
+docker push $DOCKER_ID_USER/udacity-weather-app:$BUILDID
